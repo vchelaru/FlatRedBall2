11796b03135e66414a32a526126b13545ef10342
Victor Chelaru <VicChelaru@gmail.com>
2026-04-25 07:40:39 -0600

    First pass dotnet template for desktop

---
+++ templates/frb2-desktop/MyGame.Common/Screens/GameScreen.cs
@@ -0,0 +1,18 @@
+using FlatRedBall2;
+
+namespace MyGame.Screens;
+
+public class GameScreen : Screen
+{
+    public override void CustomInitialize()
+    {
+    }
+
+    public override void CustomActivity(bool firstTimeCalled)
+    {
+    }
+
+    public override void CustomDestroy()
+    {
+    }
+}
